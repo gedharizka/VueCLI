b526eb417d70bbe0428b40734c75f67a10eb581a
gedha rizka <gedharizka@gmail.com>
2020-02-23 18:06:50 +0700

    V-for and V-key

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,37 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h3>What I do Today</h3>
+    <div class="pekerjaan-rumah">
+
+      <div v-for="tugasSaya in tugas" v-bind:key="tugasSaya.id" class="tugas">
+        <p>{{tugasSaya.nama}}</p>
+      </div>
+
+    </div>
   </div>
 </template>
+
+<script>
+  export default {
+    name:'About',
+    data : function(){
+      return{
+        tugas:[
+          {
+            id:1,
+            nama :'membersihkan kamar'
+          },
+          {
+            id:2,
+            nama :'Jalan-jalan'
+          }
+        ]
+      }
+    },
+  }
+
+</script>
+
+<style  scoped>
+
+</style>
--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -6,8 +6,8 @@
           <div class="media text-left">
             <img class="user-pic mr-3" src="../assets/logo.png" alt="Generic placeholder image">
             <div class="media-body">
-              <h5 class="mt-0">Gedha Rizka</h5>
-              <p> Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.</p>
+              <h5 class="mt-0">{{firstName}} {{lastName}}</h5>
+              <p>{{description}}</p>
             </div>
           </div>
         </div>
@@ -23,6 +23,13 @@ import 'bootstrap/dist/css/bootstrap.css'
 
 export default {
   name: 'Profile',
+  data: function(){
+    return{
+      firstName : 'Gedha',
+      lastName : 'Rizka',
+      description :'Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.'
+    }
+  },
   components: {
 
   }
